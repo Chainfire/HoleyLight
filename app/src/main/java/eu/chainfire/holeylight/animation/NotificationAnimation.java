cfe2ebcef02136f386d7c7d6f4a0443f3b5794fe
Jorrit Jongma <git@jongma.org>
2021-02-19 21:00:43 +0100

    Samsung: Unholey Light: gravity=top in TSP area, reduces jumping around on taps

--- app/src/main/java/eu/chainfire/holeylight/animation/NotificationAnimation.java
+++ app/src/main/java/eu/chainfire/holeylight/animation/NotificationAnimation.java
@@ -351,7 +351,13 @@ public class NotificationAnimation implements Settings.OnSettingsChangedListener
 
                     // Apply
                     left = tspRect.centerX() - (squareSize / 2f);
-                    top = tspRect.centerY() - (squareSize / 2f);
+                    if (Manufacturer.isSamsung()) {
+                        // Samsung: small screen area, stay at the top in case size changes when tapped, looks better
+                        top = tspRect.top;
+                    } else {
+                        // Google: stay in center of found area
+                        top = tspRect.centerY() - (squareSize / 2f);
+                    }
                     width = squareSize;
                     height = squareSize;
                 } else {
